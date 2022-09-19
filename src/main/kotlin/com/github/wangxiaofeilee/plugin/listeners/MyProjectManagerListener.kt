236238e8192823aac8c90fba5374ab2d16d055cb
GitHub Action <action@github.com>
2022-09-19 06:52:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangxiaofeilee/plugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wangxiaofeilee/plugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wangxiaofeilee.plugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wangxiaofeilee.plugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
